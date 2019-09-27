0a087c8a49bb1c87c28f0f8512efc015055210f3
shinji46kuro <shinji.46.kuro@gmail.com>
2019-09-27 21:37:16 +0900

    Initial Commit

---
+++ SourcerySample/SourcerySample.h
@@ -0,0 +1,19 @@
+//
+//  SourcerySample.h
+//  SourcerySample
+//
+//  Created by Shinji Kurosawa on 2019/09/27.
+//  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SourcerySample.
+FOUNDATION_EXPORT double SourcerySampleVersionNumber;
+
+//! Project version string for SourcerySample.
+FOUNDATION_EXPORT const unsigned char SourcerySampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SourcerySample/PublicHeader.h>
+
+
