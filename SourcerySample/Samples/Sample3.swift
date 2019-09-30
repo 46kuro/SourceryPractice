dbffa77d73cf1db3ce1907b22c35555381af150a
shinji46kuro <shinji.46.kuro@gmail.com>
2019-10-01 07:38:43 +0900

    Sampleプロジェクトの完了

--- SourcerySample/Samples/Sample3.swift
+++ SourcerySample/Samples/Sample3.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 /// argsに紐づくPropertyを生成するクラス
-/// sourcery --sources Samples/Sample3.swift --templates Stencils/Sample3.stencil --output Samples/Sample3.swift
+/// sourcery --sources Samples/Sample3.swift --templates Stencils/Sample3.stencil --output Samples/Sample3.swift --args title=String,subTitle=String
 /// を行ったときの結果を表示
 class Sample3: UIView {
 
+    // sourcery:inline:Sample3.TemplateName
+    @IBOutlet private var subTitleLabel: UILabel!
+    @IBOutlet private var titleLabel: UILabel!
+
+    public var subTitle: String? {
+        get {
+            subTitleLabel.text
+        }
+        set {
+            subTitleLabel.text = newValue
+            subTitleLabel.isHidden = newValue == nil 
+        }
+    }
+
+    public var title: String? {
+        get {
+            titleLabel.text
+        }
+        set {
+            titleLabel.text = newValue
+            titleLabel.isHidden = newValue == nil 
+        }
+    }
+    // sourcery:end
 }
