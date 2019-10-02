1b99b132d318bf3dbf9613a4531682d042ef8b74
shinji46kuro <shinji.46.kuro@gmail.com>
2019-10-02 18:43:37 +0900

    Sourceryの内部構造を理解するためにProject作成

---
+++ MockSourcery/MockSourcery/MockSourcery.h
@@ -0,0 +1,19 @@
+//
+//  MockSourcery.h
+//  MockSourcery
+//
+//  Created by Shinji Kurosawa on 2019/10/02.
+//  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MockSourcery.
+FOUNDATION_EXPORT double MockSourceryVersionNumber;
+
+//! Project version string for MockSourcery.
+FOUNDATION_EXPORT const unsigned char MockSourceryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockSourcery/PublicHeader.h>
+
+
