f7caea0aa4e21e60988575d03955ce001272dad6
shinji46kuro <shinji.46.kuro@gmail.com>
2019-10-03 20:32:22 +0900

    https://dev.to/ceri_anneblog/how-to-make-a-command-line-tool-in-xcode-2f81

---
+++ SampleCommandLineTool/SampleCommandLineTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SampleCommandLineTool
+//
+//  Created by Shinji Kurosawa on 2019/10/03.
+//  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
