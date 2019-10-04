df8857f0968a090648ac188d5a105e05139e0548
shinji46kuro <shinji.46.kuro@gmail.com>
2019-10-04 17:52:15 +0900

    Install Commander & Catch Arguments

--- SampleCommandLineTool/SampleCommandLineTool/main.swift
+++ SampleCommandLineTool/SampleCommandLineTool/main.swift
@@ -6,7 +6,17 @@
 //  Copyright © 2019 Shinji Kurosawa. All rights reserved.
 //
 
+import Commander
 import Foundation
 
 print("Hello, World!")
 
+command(
+    Option<String?>("hoge", nil, description: "Path to output. File or Directory. Default is current path.")
+) { hoge in
+    if let hoge = hoge {
+        print(hoge)
+    } else {
+        print("invalid parameter.")
+    }
+}.run()
